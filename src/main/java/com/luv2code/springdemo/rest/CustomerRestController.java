705c5bf69cf95840215be5499d5f0fb935d59a3c
Litsakis <litsakis@outlook.com>
2022-10-18 17:14:43 +0300

    NEW - Update Customer with new update Method via JSON

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,7 +62,18 @@ public class CustomerRestController {
 			return theCustomer;
 		}
 		
+	 // mapping for POST /customers 
+		@PutMapping("/customers")
+		public Customer updateCustomer(@RequestBody Customer theCustomer) {
+			
+		 
+			customerService.saveCustomer(theCustomer);
+			
 		
+			
+			return theCustomer;
+		}
+				
 }
 
 
